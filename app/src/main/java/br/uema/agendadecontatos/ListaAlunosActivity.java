b009dd2a05c9bf05b80d6c85fcbae341def8a083
Alfredo <dpd.12qwas>
2017-07-19 00:18:01 -0300

    aula 3

--- app/src/main/java/br/uema/agendadecontatos/ListaAlunosActivity.java
+++ app/src/main/java/br/uema/agendadecontatos/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.uema.agendadecontatos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -12,11 +15,20 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos); // seta e guarda as referencias
 
-        String[] alunos = {"Alfredo", "Ronaldo", "Jeferson"};
+        String[] alunos = {"Karol M", "Jhordan", "Jessica", "Alfredo", "Ronaldo", "Jeferson", "Rogério"};
         ListView listaAluno = (ListView) findViewById(R.id.lista_alunos); // obtemos a ferencia de  algum componente criado na view baseado no atributo ID
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos); // adapta objetos para seres mostrados na view
 
         listaAluno.setAdapter(adapter); // seta o adapter para view referenciada
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
 
     }
 }
