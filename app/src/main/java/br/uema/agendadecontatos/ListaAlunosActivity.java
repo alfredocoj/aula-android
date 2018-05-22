eaf24107f250b6b058f79b803e34e9c4226f7f7d
Alfredo <dpd.12qwas>
2018-05-22 10:09:54 -0300

    buscando listagem automaticamente do BD.

--- app/src/main/java/br/uema/agendadecontatos/ListaAlunosActivity.java
+++ app/src/main/java/br/uema/agendadecontatos/ListaAlunosActivity.java
@@ -24,12 +24,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         List<Aluno> alunos = alunoDAO.buscaAluno();
 
-        //String[] alunos = {"Jhordan", "Jessica", "Alfredo", "Ronaldo", "Jeferson", "Rogério"};
-        ListView listaAluno = (ListView) findViewById(R.id.lista_alunos); // obtemos a ferencia de  algum componente criado na view baseado no atributo ID
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos); // adapta objetos para seres mostrados na view
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos); // adapta objetos para seres mostrados na view
-
-        listaAluno.setAdapter(adapter); // seta o adapter para view referenciada
+        carregarLista();
 
         Button novoAluno = (Button) findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +35,24 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregarLista();
+    }
+
+    public void carregarLista(){
+        AlunoDAO alunoDAO = new AlunoDAO(this);
+
+        List<Aluno> alunos = alunoDAO.buscaAluno();
+
+        //String[] alunos = {"Jhordan", "Jessica", "Alfredo", "Ronaldo", "Jeferson", "Rogério"};
+        ListView listaAluno = (ListView) findViewById(R.id.lista_alunos); // obtemos a ferencia de  algum componente criado na view baseado no atributo ID
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos); // adapta objetos para seres mostrados na view
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos); // adapta objetos para seres mostrados na view
+
+        listaAluno.setAdapter(adapter); // seta o adapter para view referenciada
     }
 }
